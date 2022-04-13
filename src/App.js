84a0398a980abb9440c08eca003655c79afedc67
Altin Selimi <altin.selimi@ndbit.net>
2022-04-13 17:04:33 +0200

    Copmany-name Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Reservations from "./data/db.json"
+
+console.log("dbDataaaa", Reservations.reservations[0].time_slots[0, 9].start_time)
+// console.log(Reservations.item.time_slots, "itemsloots")
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Box style={{ marginTop: 20 }}>
+        {Reservations.reservations.map((item, i) => (
+          <TextField key={i}
+            style={{ marginRight: 50, textAlignLast: 'center' }}
+            id="outlined-read-only-input"
+            defaultValue={item.name}
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+        ))}
+      </Box>
+
+      <Box style={{ marginTop: 20 }}>
+        <TextField
+          style={{ marginRight: 50, textAlignLast: 'center' }}
+          disabled
+          id="outlined-disabled"
+          // label="Disabled"
+          defaultValue="Value Selected"
+        />
+        <TextField
+          style={{ marginRight: 50, textAlignLast: 'center' }}
+          disabled
+          id="outlined-disabled"
+          // label="Disabled"
+          defaultValue="Value Selected"
+        />
+        <TextField
+          style={{ marginRight: 50, textAlignLast: 'center' }}
+          disabled
+          id="outlined-disabled"
+          // label="Disabled"
+          defaultValue="Value Selected"
+        />
+      </Box>
+
+      <Box style={{ marginTop: 60 }}>
+        <Box style={{ display: 'inline-grid' }}>
+          {Reservations.reservations[0].time_slots.map((item, i) => (
+            < TextField key={i}
+              style={{ marginRight: 50, textAlignLast: 'center' }}
+              id="outlined-read-only-input"
+              defaultValue={item}
+              InputProps={{
+                readOnly: true,
+              }}
+            />
+          ))}
+        </Box>
+        <Box style={{ display: 'inline-grid' }}>
+          {Reservations.reservations[1].time_slots.map((item, i) => (
+            < TextField key={i}
+              style={{ marginRight: 50, textAlignLast: 'center' }}
+              id="outlined-read-only-input"
+              defaultValue={item.start_time}
+              InputProps={{
+                readOnly: true,
+              }}
+            />
+          ))}
+        </Box>
+        <Box style={{ display: 'inline-grid' }}>
+          {Reservations.reservations[2].time_slots.map((item, i) => (
+            < TextField key={i}
+              style={{ marginRight: 50, textAlignLast: 'center' }}
+              id="outlined-read-only-input"
+              defaultValue={item}
+              InputProps={{
+                readOnly: true,
+              }}
+            />
+          ))}
+        </Box>
+      </Box>
+
+    </div >
   );
 }
 
